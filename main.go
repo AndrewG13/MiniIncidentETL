0e1e8b1017d2e8732a0669f32768d54691016987
AndrewG13 <andrew.giardina1@marist.edu>
2022-02-21 08:26:55 -0500

    JSON file access #1

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-  // add more
   "fmt"
+  //"encoding/json"
+  "os"
 )
 
 // Notes:
@@ -10,14 +11,30 @@ import (
 // https://pkg.go.dev/flag
 
 // define struct for JSON data, may not need
-type incident struct {
-  Id int
-  Name string
-  Discovered string
-  Description string
-  Status string
+type Incident struct {
+  Id int `json:"id"`
+  Name string `json:"name"`
+  Discovered string `json:"discovered"`
+  Description string `json:"description"`
+  Status string `json:"status"`
 }
 
 func main() {
-    fmt.Println("Hello World! from Andrew Giardina")
+    // Open the JSON data file for usage
+    jsonFile, err := os.Open("input/data.json")
+
+    // if file not found, print error
+    if err != nil {
+      fmt.Println("Error Accessing File:\n")
+      fmt.Println(err)
+      fmt.Println("\nEnsure File is in 'input' folder & named 'data.json'.")
+    } else {
+      // file access successful!
+      fmt.Println("File Successfully Accessed")
+      // defer closing the file to allow parsing
+      defer jsonFile.Close()
+    }
+
+
+    //fmt.Println("Hello World! from Andrew Giardina")
 }
