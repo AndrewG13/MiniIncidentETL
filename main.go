d3bc1118710a3cc640d8c56bb87cec0814f6cbf3
AndrewG13 <andrew.giardina1@marist.edu>
2022-02-20 23:52:37 -0500

    Created basic struct for JSON

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+  // add more
   "fmt"
 )
+
+// Notes:
+// flag for CLI
+// https://pkg.go.dev/flag
+
+// define struct for JSON data, may not need
+type incident struct {
+  Id int
+  Name string
+  Discovered string
+  Description string
+  Status string
+}
 
 func main() {
     fmt.Println("Hello World! from Andrew Giardina")
